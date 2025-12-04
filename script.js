a70af54aa227fe494fd9690a37c55cf202769029
maorpl <t.bajorowy@gmail.com>
2025-12-04 21:47:20 +0100

    Dodanie samej strony cn
    Jest to tylko baza, jeszcze głównej funkcjonalności nie ma ten kalkulator. Jest po prostu layout i tyle...
    
    NIE, NIE ZAPOMNIAŁEM O KALKULATORZE MIEJSC ZEROWYCH.

---
+++ script.js
@@ -0,0 +1,5 @@
+const inputs = document.getElementsByClassName("input");
+const ioShit = document.querySelector(".ioShit");
+
+// basically potrzeba napisać cały kod, który będzie powodował wpisywanie
+// się liczb na pole tekstowe, nwm jak jo to uczyni...
